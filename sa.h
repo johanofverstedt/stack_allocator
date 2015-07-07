8a782d414cebc1b07cde704a825e01e3043c56d0
Johan Öfverstedt <johan.ofverstedt@gmail.com>
2015-07-07 04:01:58 +0200

    Fixed the header file.

--- sa.h
+++ sa.h
@@ -9,23 +9,23 @@ extern "C" {
 //
 // core allocator functions
 //
-void bft_stack_init(void* a, size_t size);
-void* bft_stack_alloc(void* a, size_t size);
-void* bft_stack_realloc(void* a, void* ptr, size_t new_size);
-void bft_stack_free(void* a, void* ptr);
+void sa_stack_init(void* a, size_t size);
+void* sa_stack_alloc(void* a, size_t size);
+void* sa_stack_realloc(void* a, void* ptr, size_t new_size);
+void sa_stack_free(void* a, void* ptr);
 
 //
 // stack allocator functions
 //
-void bft_stack_push(void* a);
-void bft_stack_pop(void* a);
+void sa_stack_push(void* a);
+void sa_stack_pop(void* a);
 
 // debugging routines
 
-void bft_stack_debug_print_meta(void* a);
-size_t bft_stack_meta_hash(void* a);
+void sa_stack_debug_print_meta(void* a);
+size_t sa_stack_meta_hash(void* a);
 
-void bft_stack_print_stuff();
+void sa_stack_print_stuff();
 
 #ifdef __cplusplus
 }
